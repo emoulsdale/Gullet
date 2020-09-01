0466221e72b433086230da25ca911df9f0e6d92c
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-09-01 13:53:52 +0100

    Removed gullet.json, added test detection

--- addons/gullet/export.gd
+++ addons/gullet/export.gd
@@ -1,21 +1,50 @@
 tool
 extends EditorExportPlugin
 
-var config: Dictionary
+const root_test_dir = "res://test"
 
 
-func find_tests() -> Array:
-	var test_dirs: Array = config["test_dirs"]
-	for dir in test_dirs:
-		print(dir)
-	return []
+func open_test_dir(test_dir_path: String) -> Directory:
+	var test_dir := Directory.new()
+	test_dir.open(test_dir_path)
+	return test_dir
+
+
+func process_elements(test_dir: Directory, test_dir_path: String, test_paths: Array) -> Array:
+	while true:
+		var element := test_dir.get_next()
+		if not element:
+			break
+		var element_path := "%s/%s" % [test_dir_path, element]
+		if test_dir.current_is_dir():
+			test_paths = get_tests_in_dir(element_path, test_paths.duplicate())
+		else:
+			test_paths.append(element_path)
+	return test_paths
+
+
+func get_tests_in_dir(test_dir_path: String, test_paths: Array) -> Array:
+	var test_dir := open_test_dir(test_dir_path)
+	test_dir.list_dir_begin(true, true)
+	test_paths += process_elements(test_dir, test_dir_path, test_paths.duplicate())
+	test_dir.list_dir_end()
+	return test_paths
+
+func get_tests() -> Array:
+	var test_paths := get_tests_in_dir(root_test_dir, [])
+	return test_paths
+
+
+func add_test(test_path: String) -> void:
+	var pool_byte_array := PoolByteArray()
+	add_file(test_path, pool_byte_array, false)
 
 
 func add_tests() -> void:
-	print("Debug export, adding tests...")
-	var tests := find_tests()
-	var pool_byte_array := PoolByteArray()
-	add_file("res//test/unit.gd", pool_byte_array, false)
+	var test_paths := get_tests()
+	print("Found %d tests." % len(test_paths))
+	for test_path in test_paths:
+		add_test(test_path)
 
 
 func _export_begin(_features: PoolStringArray, is_debug: bool, _path: String, _flags: int) -> void:
--- addons/gullet/gullet.gd
+++ addons/gullet/gullet.gd
@@ -1,42 +1,13 @@
 tool
 extends EditorPlugin
 
-const config_path = "res://gullet.json"
-var config: Dictionary
-
-func read_config_file() -> String:
-	var config_file := File.new()
-	config_file.open(config_path, File.READ)
-	var json_string := config_file.get_as_text()
-	config_file.close()
-	return json_string
-
-
-func get_json_parse_result(json_string: String) -> JSONParseResult:
-	var json_parse_result := JSON.parse(json_string)
-	return json_parse_result
-
-
-func check_json_parse_result(json_parse_result: JSONParseResult) -> void:
-	if json_parse_result.error != OK:
-		push_error(json_parse_result.error_string)
-
-
-func get_config() -> Dictionary:
-	var json_string := read_config_file()
-	var json_parse_result := get_json_parse_result(json_string)
-	check_json_parse_result(json_parse_result)
-	return json_parse_result.result as Dictionary
-
 
 func setup_export_plugin() -> void:
 	var export_plugin: EditorExportPlugin = preload("export.gd").new()
-	export_plugin.config = config
 	add_export_plugin(export_plugin)
 
 
 func _enter_tree() -> void:
-	config = get_config()
 	setup_export_plugin()
 
 
