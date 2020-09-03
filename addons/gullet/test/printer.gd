3326f00754a058e1813a7de6a091674af62c93e3
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-09-03 15:06:27 +0100

    Modified printer output

--- addons/gullet/test/printer.gd
+++ addons/gullet/test/printer.gd
@@ -3,6 +3,7 @@ extends Node
 
 const FAILURE_ANSI_COLOR = "[1;31m"
 const SUCCESS_ANSI_COLOR = "[1;32m"
+const INFO_ANSI_COLOR = "[1;33m"
 const RESET_ANSI_COLOR = "[1;0m"
 
 var escape_string := PoolByteArray([0x1b]).get_string_from_ascii()
@@ -18,21 +19,31 @@ var success_start := "%s%sSUCCESS%s%s" % [
     escape_string,
     RESET_ANSI_COLOR,
 ]
+var info_start := "%s%s" % [
+    escape_string,
+    INFO_ANSI_COLOR,
+]
+var info_end := "%s%s" % [
+    escape_string,
+    RESET_ANSI_COLOR
+]
 
 
 func print_failure(test_file_path: String, test_method: String,
 failure_string: String) -> void:
-    print("%s - Test method '%s' of file '%s': %s" % [
+    print("%s -> '%s' of '%s': %s%s%s" % [
         failure_start,
         test_method,
         test_file_path,
+        info_start,
         failure_string,
+        info_end,
     ])
 
 
 # TODO rename success->pass in everything!
 func print_success(test_file_path: String, test_method: String) -> void:
-    print("%s - Test method '%s' of file '%s'" % [
+    print("%s -> '%s' of '%s'" % [
         success_start,
 		test_method,
 		test_file_path,
