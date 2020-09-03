7b27a1b5e67bd4378fafb936144c63822dc852d3
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-09-03 15:58:45 +0100

    Corrected order of running tests and disposal

--- ci.gd
+++ ci.gd
@@ -5,7 +5,7 @@ const test_container_path= "res://addons/gullet/test/container.gd"
 
 func _init():
     var test_container: Node = preload(test_container_path).new()
-    test_container.dispose()
     test_container.run_all_tests()
+    test_container.dispose()
     test_container.queue_free()
     quit()
