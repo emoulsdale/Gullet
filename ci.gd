b070d5eb777673330c9c6b56509b99f59e41e1b5
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-09-03 15:35:39 +0100

    Added CI

---
+++ ci.gd
@@ -0,0 +1,10 @@
+extends SceneTree
+
+const test_container_path= "res://addons/gullet/test/container.gd"
+
+
+func _init():
+	var test_container: Node = preload(test_container_path).new()
+	test_container.dispose()
+	test_container.queue_free()
+	quit()
