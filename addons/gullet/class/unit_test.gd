23193f281d2ad6f3cbd0f71b792f41c913461343
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-09-03 14:14:26 +0100

    Fixed bug in assert_eq of unit_test.gd

--- addons/gullet/class/unit_test.gd
+++ addons/gullet/class/unit_test.gd
@@ -5,5 +5,6 @@ extends "res://addons/gullet/class/base_test.gd"
 func assert_eq(int1: int, int2: int) -> void:
 	if int1 == int2:
 		emit_signal("test_completed", "")
-	var failure_format := "The provided integers (%d, %d) are not equal"
-	emit_signal("test_completed", failure_format % [int1, int2])
+	else:
+		var failure_format := "The provided integers (%d, %d) are not equal"
+		emit_signal("test_completed", failure_format % [int1, int2])
