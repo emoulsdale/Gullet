e5f5fb17fa20ad26356ce19f3e39b941b02d5201
Edward Moulsdale <eddie.moulsdale@gmail.com>
2020-09-03 15:46:38 +0100

    Actually running tests now

--- ci.gd
+++ ci.gd
@@ -4,7 +4,8 @@ const test_container_path= "res://addons/gullet/test/container.gd"
 
 
 func _init():
-	var test_container: Node = preload(test_container_path).new()
-	test_container.dispose()
-	test_container.queue_free()
-	quit()
+    var test_container: Node = preload(test_container_path).new()
+    test_container.dispose()
+    test_container.run_all_tests()
+    test_container.queue_free()
+    quit()
