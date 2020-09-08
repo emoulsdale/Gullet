a39378d52dcf429acef767263bee5d8956ab175d
Edward Moulsdale <--global>
2020-09-08 14:19:27 +0100

    Named some files more explicitly

--- addons/gullet/tester/runner.gd
+++ addons/gullet/tester/runner.gd
@@ -22,7 +22,7 @@ func report_failure(failure: String) -> void:
 	emit_signal("processing_finished")
 
 
-func _run_test(test_file: Node, test_method: String) -> void:
+func _run_test_method(test_file: Node, test_method: String) -> void:
 	_current_test_method = test_method
 	funcref(test_file, test_method).call_func()
 	yield(self, "processing_finished")
@@ -35,10 +35,10 @@ func _setup_test_file(test_file_path: String) -> Node:
 	return test_file
 
 
-func _run_tests_in_file(test_file_path: String) -> void:
+func _run_all_test_methods_in_file(test_file_path: String) -> void:
 	var test_file := _setup_test_file(test_file_path)
 	for test_method in FINDER.get_test_methods_in_file(test_file):
-		_run_test(test_file, test_method)
+		_run_test_method(test_file, test_method)
 	test_file.queue_free()
 
 
@@ -47,4 +47,4 @@ func run_all_tests() -> void:
 	# finished (i.e. stopped due to failure, or passed all tests)
 	var test_file_paths := FINDER.get_test_file_paths()
 	for test_file_path in test_file_paths:
-		_run_tests_in_file(test_file_path)
+		_run_all_test_methods_in_file(test_file_path)
