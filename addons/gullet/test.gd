f4e03415cbb6ef61aacab296d915dd1363364e79
Edward Moulsdale <--global>
2020-09-08 13:02:47 +0100

    Added reference to runner in test.gd

--- addons/gullet/test.gd
+++ addons/gullet/test.gd
@@ -1,11 +1,25 @@
 extends Node
 
-signal test_completed(failure_string)
+# TODO refactor order of members to be consistent with gdscript guidelines
+var runner: Node
+
+# TODO refactor to use xUnit classnames
+
+
+func report_pass() -> void:
+	# TODO should this _if_ statement be here?
+	if runner:
+		runner.report_pass()
+
+
+func report_failure(failure: String) -> void:
+	if runner:
+		runner.report_failure(failure)
 
 
 func assert_eq(int1: int, int2: int) -> void:
 	if int1 == int2:
-		emit_signal("test_completed", "")
+		report_pass()
 	else:
 		var failure_format := "The provided integers (%d, %d) are not equal"
-		emit_signal("test_completed", failure_format % [int1, int2])
+		report_failure(failure_format % [int1, int2])
--- addons/gullet/tester/runner.gd
+++ addons/gullet/tester/runner.gd
@@ -3,6 +3,8 @@ extends Node
 
 const UTILS := preload("res://addons/gullet/tester/finder.gd")
 
+# TODO prepend "_" to private members
+
 signal processing_finished
 signal test_failed(test_file_path, test_method, failure_string)
 signal test_passed(test_file_path, test_method)
@@ -12,21 +14,30 @@ var current_test_method: String
 var excluded_methods := get_excluded_method_names()
 
 
-func run_test(test_file: Node, test_file_path: String,
-		test_method: String) -> void:
-    current_test_file_path = test_file_path
-    current_test_method = test_method
-    funcref(test_file, test_method).call_func()
-    yield(self, "processing_finished")
+func report_pass() -> void:
+	emit_signal("test_passed", current_test_file_path, current_test_method)
+	emit_signal("processing_finished")
+
+
+func report_failure(failure: String) -> void:
+	emit_signal("test_failed", current_test_file_path, current_test_method,
+			failure)
+	emit_signal("processing_finished")
+
+
+func run_test(test_file: Node, test_method: String) -> void:
+	current_test_method = test_method
+	funcref(test_file, test_method).call_func()
+	yield(self, "processing_finished")
 
 
 # this must be called every time the base test script is updated!
 func get_excluded_method_names() -> Array:
-    var base_test_file := preload("res://addons/gullet/test.gd")
-    var base_test_file_instance: Node = base_test_file.new()
-    var excluded_method_names := get_method_names(base_test_file_instance)
-    base_test_file_instance.queue_free()
-    return excluded_method_names
+	var base_test_file := preload("res://addons/gullet/test.gd")
+	var base_test_file_instance: Node = base_test_file.new()
+	var excluded_method_names := get_method_names(base_test_file_instance)
+	base_test_file_instance.queue_free()
+	return excluded_method_names
 
 
 func get_method_names(node: Node) -> Array:
@@ -54,12 +65,17 @@ func process_test_result(failure_string: String) -> void:
 	emit_signal("processing_finished")
 
 
-func run_tests_in_file(test_file_path: String) -> void:
+func setup_test_file(test_file_path: String) -> Node:
 	var test_file: Node = load(test_file_path).new()
-	test_file.connect("test_completed", self, "process_test_result")
+	current_test_file_path = test_file_path
+	test_file.runner = self
+	return test_file
+
+
+func run_tests_in_file(test_file_path: String) -> void:
+	var test_file := setup_test_file(test_file_path)
 	for test_method in get_test_methods(test_file):
-		run_test(test_file, test_file_path, test_method)
-	test_file.disconnect("test_completed", self, "process_test_result")
+		run_test(test_file, test_method)
 	test_file.queue_free()
 
 
